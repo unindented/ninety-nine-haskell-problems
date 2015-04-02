127c123756eb5043cebfac5035c9f41c4cf944a8
Daniel Perez Alvarez <unindented@gmail.com>
2015-04-03 01:40:09 +0200

    Problem 2

--- src/Problems1To10.hs
+++ src/Problems1To10.hs
@@ -11,3 +11,14 @@ module Problems1To10 where
 {-# ANN myLast "HLint: ignore Use last" #-}
 myLast :: [a] -> a
 myLast = head . reverse
+
+-- | Problem 2
+-- Find the last but one element of a list.
+--
+-- >>> myButLast [1,2,3,4]
+-- 3
+--
+-- >>> myButLast ['x','y','z']
+-- 'y'
+myButLast :: [a] -> a
+myButLast = last . init
